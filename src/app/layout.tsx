da8242dbef8826f19ec171f7ce40f77b756649c7
Michael Ross Tarr <mrmrmtarr@gmail.com>
2024-04-28 22:03:40 +0200

    speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Outfit } from 'next/font/google'
 import './globals.css'
 import config from '../config.json';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const outfit = Outfit({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={outfit.className}>{children}</body>
     </html>
   )
