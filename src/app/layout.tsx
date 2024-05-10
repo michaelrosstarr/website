9202efbf71cbe960361cb76cebd49a93f61e302b
Michael Ross Tarr <mrmrmtarr@gmail.com>
2024-05-11 00:29:18 +0200

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Outfit } from 'next/font/google'
 import './globals.css'
 import config from '../config.json';
+import { Analytics } from "@vercel/analytics/react"
 
 const outfit = Outfit({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Analytics />
       <body className={outfit.className}>{children}</body>
     </html>
   )
